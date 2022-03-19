18bff418593187381f1b4adea02d43eaaf49178d
ivanarganda96 <76394300+ivanarganda96@users.noreply.github.com>
2022-03-19 01:05:23 +0100

    Update dbSocialNetwork.sql

--- dbSocialNetwork.sql
+++ dbSocialNetwork.sql
@@ -1,1 +1,19 @@
+create database if not exists socialNetwork default character set utf8 collate utf8_spanish_ci;
+
+use socialNetwork;
+
+create table if not exists users(
+  
+  
+) ENGINE = InnoDB default charset=utf8 collate=utf8_unicode_ci;
+
+create table if not exists usersImages(
+
+) ENGINE = InnoDB default charset=utf8 collate=utf8_unicode_ci;
+
+create table if not exists imagesUser(
+
+
+) ENGINE = InnoDB default charset=utf8 collate=utf8_unicode_ci;
+
 
